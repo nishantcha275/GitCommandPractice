b09eb57b909f0ada83fbfaea1f5a18bbaa75846d
Nishant <Nishant@DESKTOP-D0HDFK6>
2024-05-31 15:33:24 +0530

    add function

--- GitPracticecode/Program.cs
+++ GitPracticecode/Program.cs
@@ -8,9 +8,12 @@ namespace GitPracticecode
 {
     internal class Program
     {
+        public static void add(int a, int b)
+        { a += b; }
         static void Main(string[] args)
         {
             Console.WriteLine("this is main branch");
+            add(1, 2);
             Console.ReadLine();
         }
     }
